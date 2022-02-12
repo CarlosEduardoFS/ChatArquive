9c31544686d565ef9931b0a5fedc8723dd088dd6
eduardo <eduardofs_2016@outlook.com>
2022-02-12 18:58:12 -0300

    exception classes

---
+++ src/model/exception/ServiceExeption.java
@@ -0,0 +1,10 @@
+package model.exception;
+
+public class ServiceExeption extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+    public ServiceExeption(String message) {
+        super(message);
+    }
+}
